d92dea6c550978035a8ae9924ad6bc4ce989ab89
Chris Russo <ccchris.russo@gmail.com>
2017-05-14 20:00:37 -0400

    Update README, complete API, add requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Spotify Curator
+
+Author: Chris Russo
+
+## Setup
+
+Before running this Flask API, install the project requirements by executing `$ pip install -r requirements.txt` in your shell.
+
+Run the API locally by executing `$ python app.py` in your shell from the root directory of this project.
+
+## Endpoints
+
+Once the API is running locally, you can submit POST requests to the Playlist Maker endpoint (/make_playlist) with the following command:
+
+```
+curl -X POST \
+  http://127.0.0.1:5000/make_playlist \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' \
+  -H 'postman-token: 327406e6-59df-8237-6a53-c61c33995a2c' \
+  -F 'text=<KEYWORDS_GO_HERE>'
+```
+
+Replace the value `<KEYWORDS_GO_HERE>` in the 'text' key of the form body with whatever keywords you'd like.
+
+
+
--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 """
 Spotify Curator
 
-Spotify Curator is a Flask API that generates a playlist of track titles 
-(Spotify URLs) from text submitted by a user.
+Spotify Curator is a Flask API with a single endpoint. A POST request containing 
+text made to the Playlist Maker endpoint ('make_playlist') will return a 
+playlist, or list of Spotify URLs. 
 
 For example, the text "If I can't let it go out of my mind" may return the
 playlist:
 
-  1. http://open.spotify.com/track/6mcu7D7QuABVwUGDwovOEh
-  2. http://open.spotify.com/track/5ZRxxnab9kLUqZPzoelgGP
-  3. http://open.spotify.com/track/3L0bYyI0FHRiD1xZfbZedz
+    {
+      "tracks": [
+        "https://open.spotify.com/track/7JeKXMQKm6GoLGTkNy2jZ0",
+        "https://open.spotify.com/track/5ZRxxnab9kLUqZPzoelgGP",
+        "https://open.spotify.com/track/16uXqmSMAOl0MxtgVQTGeH"
+      ]
+    }
 
 
 Spotify API Endpoint Reference: Search
@@ -19,19 +24,14 @@ https://developer.spotify.com/web-api/search-item/
 # "https://api.spotify.com/v1/search?q="at+your+funeral"+artist:saves+the+day&type=track"
 
 
-API order of operations:
-  - Clean text input, i.e. keep alphanumeric characters only
-  - Create URL string from text
-  - Use Spotify API Endpoint Search to search for track titles like text input
-  - Parse through search results and key-value pairs of track titles and
-    external Spotify URL
-  - Ensure output playlist track titles matches order of text input
-
 """
 
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Resource, Api, reqparse
+import requests
 import json
+import random
+import re
 
 # Instantiate Flask app and API objects
 app = Flask(__name__)
@@ -44,8 +44,14 @@ parser = reqparse.RequestParser(bundle_errors=True)
 parser.add_argument('text', type=str, location='form')
 
 
-# Regex substitute any non-alphanumeric characters with a space
-# re.sub('[^a-zA-Z0-9_]', ' ', x.lower()).split(' ')
+def create_url(query_string: str) -> str:
+    url = 'https://api.spotify.com/v1/search?q="%s"&type=track' % query_string
+    return url
+
+
+def search_tracks(url: str) -> dict:
+    response = requests.get(url)
+    return json.loads(response.text)
 
 
 # Create playlist maker class
@@ -58,10 +64,84 @@ class PlaylistMaker(Resource):
     """
 
     def post(self):
+        # Get text from POST body
         args = parser.parse_args(strict=True)
-        response = args
+        text = args.text
 
-        return response.text + ' from space.'
+        # Remove commas and apostrophes from text and split keywords into list
+        keywords = re.sub(r'[\',]', "", text.lower()).split(' ')
+
+        # Remove empty strings from list of keywords
+        keywords = list(filter(None, keywords))
+
+        # Create an empty list of remaining keywords to append to later on
+        remaining_keywords = []
+
+        # Create an empty list to append Spotify track URLs later on. This list
+        # will be returned when a POST request is made to the 'make_playlist'
+        # endpoint
+        playlist = {'tracks': []}
+
+        while len(keywords) > 0:
+            # Create string of keywords to compare with track titles later on
+            keywords_string = ' '.join(keywords)
+
+            # Create query string to inject into Spotify API GET request
+            query_string = '+'.join(keywords)
+
+            # Make GET request to Spotify API Search endpoint
+            url = create_url(query_string)
+            data = search_tracks(url)
+
+            # Create empty dictionary of tracks
+            tracks = {}
+
+            # Retrieve track name and track's Spotify URL from Spotify API
+            # response data items list
+            items = data['tracks']['items']
+
+            for i in range(len(items)):
+                track_i = items[i]['name']
+                spotify_url_i = items[i]['external_urls']['spotify']
+                tracks[track_i] = spotify_url_i
+
+            # If no tracks matching keywords are found, update keywords and
+            # remaining keywords lists and try again
+            if tracks == {}:
+                remaining_keywords.insert(0, keywords[-1])
+                keywords = keywords[0:-1]
+
+            # If tracks found, evaluate titles for matches with keywords
+            else:
+                # Create empty list of valid tracks from tracks dictionary
+                keep_tracks = []
+
+                # Append tracks with valid titles to list of tracks to keep
+                # NOTE: A valid title is one that is identical to the keywords
+                for title in list(tracks.keys()):
+                    if re.sub(r'[\',]', "", title.lower()) == keywords_string:
+                        keep_tracks.append(title)
+
+                # Define logic for assessing list of valid track titles
+                if len(keep_tracks) == 0:
+                    # If no identical track titles found, update keywords and
+                    # remaining keywords lists and try again
+                    remaining_keywords.insert(0, keywords[-1])
+                    keywords = keywords[0:-1]
+
+                else:
+                    # If identical track titles found, randomly select a track
+                    # from list of valid tracks
+                    keep_track = random.choice(keep_tracks)
+
+                    # Append track Spotify URL to playlist
+                    playlist['tracks'].append(tracks[keep_track])
+
+                    # Update keywords and remaining keywords lists
+                    keywords = remaining_keywords
+                    remaining_keywords = []
+
+        return jsonify(playlist)
 
 
 # Set up API source routing
